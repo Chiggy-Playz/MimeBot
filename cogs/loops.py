fb55c521be3f132fdebcd3f2d3999d931a9fd015
Chiggy-Playz <garg.chirag2004@gmail.com>
2024-11-18 14:38:15 +0530

    Add typing

--- cogs/loops.py
+++ cogs/loops.py
@@ -1,8 +1,11 @@
 from discord.ext import commands, tasks
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from bot import MimeBot
 
 class Loops(commands.Cog):
 
-    def __init__(self, bot):
+    def __init__(self, bot: MimeBot):
         self.bot = bot
     
     async def cog_load(self) -> None:
@@ -10,12 +13,12 @@ class Loops(commands.Cog):
 
     @tasks.loop(hours=12)
     async def loop(self):
-        asset_count = await self.bot.db.fetchval("SELECT COUNT(*) FROM assets")
+        asset_count = await self.bot.pool.fetchval("SELECT COUNT(*) FROM assets")
         print("Fetched asset count:", asset_count)    
     
     @loop.before_loop
     async def before_loop(self):
         await self.bot.wait_until_ready()
 
-async def setup(bot):
+async def setup(bot: MimeBot):
     await bot.add_cog(Loops(bot))
